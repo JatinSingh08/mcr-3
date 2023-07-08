d52577d09a09213dd9d3a655b3c2465f93688305
JatinSingh08 <singhjatin0812@gmail.com>
2023-07-09 00:29:27 +0530

    card ui fixed

--- src/components/CuisineItems.jsx
+++ src/components/CuisineItems.jsx
@@ -14,18 +14,18 @@ const CuisineItems = () => {
     <div>
       {restaurantCuisines?.map((restaurant) => {
         return (
-          <div className="text-start" key={restaurant.id}>
-            <Link to={`/restaurant/${restaurant?.id}`} className="text-xl font-semibold underline">
+          <div className="text-start mt-2" key={restaurant.id}>
+            <Link to={`/restaurant/${restaurant?.id}`} className="text-2xl font-semibold underline">
               Dishes by {restaurant?.name}
             </Link>
             <div className="flex gap-3">
               {restaurant?.menu?.map((item, index) => (
-                <div className="border-gray-400 p-2"
+                <div className="border-gray-400 rounded-lg mt-2"
                 key={index}
                 onClick={() => navigate(`/restaurant/${restaurant?.id}`)}
                 >
-                  <img src={item?.imgSrc} alt={item?.name} className="h-40" />
-                  <div>
+                  <img src={item?.imgSrc} alt={item?.name} className="h-40 cursor-pointer" />
+                  <div className="p-2 border">
                     <h1 className="text-lg font-medium ">{item?.name}</h1>
                     <p>{item?.price} for one</p>
                     <p>{restaurant?.name}</p>
